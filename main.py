0083e24680916a0136c8ee01a54661cc622961f1
Kern <gf4t47@gmail.com>
2020-03-21 15:28:48 -0700

    re-folder

--- main.py
+++ main.py
@@ -1,15 +1,17 @@
-from word_dictionary import dictionary
-from randomization import shuffle_list
-from format import markdown
+from pathlib import Path
+
+from data.word_dictionary import dictionary
+from src.randomization import shuffle_list
+from src.format import markdown
 import argparse
 
 
-def write_markdown(name: str) -> None:
+def write_markdown(name: str, path: str = './out') -> None:
     s = shuffle_list(dictionary[name])
     md = markdown(s)
     print(md)
 
-    with open(f'{name}.md', 'w') as f:
+    with open(Path(path, name).with_suffix('.md'), 'w') as f:
         for line in md:
             f.write(line + '\n')
 
