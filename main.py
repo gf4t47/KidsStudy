bd8bc757b7c8454045b081ca9967787fe5e914d2
Kern <gf4t47@gmail.com>
2020-03-21 15:50:31 -0700

    Open result file

--- main.py
+++ main.py
@@ -3,21 +3,25 @@ from pathlib import Path
 from data.word_dictionary import dictionary
 from src.randomization import shuffle_list
 from src.format import markdown
+from subprocess import Popen
 import argparse
 
 
-def write_markdown(name: str, path: str = './out') -> None:
+def write_markdown(name: str, path: str = './out') -> Path:
     s = shuffle_list(dictionary[name])
     md = markdown(s)
     print(md)
 
-    with open(Path(path, name).with_suffix('.md'), 'w') as f:
+    output = Path(path, name).with_suffix('.md')
+    with open(output, 'w') as f:
         for line in md:
             f.write(line + '\n')
+    return output
 
 
 dict_para = 'name'
 parser = argparse.ArgumentParser()
 parser.add_argument(dict_para)
 args = vars(parser.parse_args())
-write_markdown(args[dict_para])
+md_file = write_markdown(args[dict_para])
+Popen(['typora', md_file], close_fds=True)
